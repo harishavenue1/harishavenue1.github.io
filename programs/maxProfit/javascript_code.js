c6e7fd93e6e10fe9bd4ee51bc3e5e6a42949649e
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-12 22:20:23 +0530

    Create javascript_code.js

---
+++ programs/maxProfit/javascript_code.js
@@ -0,0 +1,21 @@
+var maxProfit = function(prices) {
+    let maxProfit = 0
+    let currentProfit = 0
+    let leftIndex = 0
+    let rightIndex = 0
+
+    while (rightIndex < prices.length) {
+        if (prices[rightIndex] > prices[leftIndex]) {
+            currentProfit = prices[rightIndex] - prices[leftIndex];
+            maxProfit = Math.max(currentProfit, maxProfit);
+        }
+        else {
+            leftIndex = rightIndex;
+        }
+        rightIndex+=1;
+    }
+    return maxProfit
+};
+
+let profit = maxProfit([7,6,4,3,1]) // -> 0, [7,1,5,3,6,4] -> 5
+console.log(profit)
