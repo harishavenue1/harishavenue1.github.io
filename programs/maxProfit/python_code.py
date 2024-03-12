b6f93be002532741754ee51e8bb8d1ee678b0d0d
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-12 22:15:00 +0530

    Create python_code.py

---
+++ programs/maxProfit/python_code.py
@@ -0,0 +1,16 @@
+def maxProfit(prices):
+    maxProfit = 0
+    leftIndex = 0
+    rightIndex = 0
+    currentProfit = 0
+    while rightIndex < len(prices):
+        if prices[rightIndex] > prices[leftIndex]:
+            currentProfit = prices[rightIndex] - prices[leftIndex]
+            maxProfit = max(currentProfit, maxProfit)
+        else:
+            leftIndex = rightIndex
+        rightIndex+=1
+    return maxProfit
+
+mProfit = maxProfit([7,1,5,3,6,4]) # -> 5, [7,6,4,3,1] -> 0
+print(mProfit)
