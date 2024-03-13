b7f4144a88a23f91e315b749a4865e76ab940a19
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-13 21:36:43 +0530

    Create java_code.java

---
+++ programs/containsDuplicate/java_code.java
@@ -0,0 +1,26 @@
+package programs.containsDuplicate;
+
+public class java_code
+{
+    public static void main(String[] args) {
+      boolean flag = containsDuplicate(new int[] {1,2,3,1});
+      System.out.println("containsDuplicate "+ flag);
+      
+      flag = containsDuplicate(new int[] {1,2,3,4});
+      System.out.println("containsDuplicate "+ flag);
+      
+      flag = containsDuplicate(new int[] {1,1,1,3,3,4,3,2,4,2});
+      System.out.println("containsDuplicate "+ flag);
+  }
+  
+  public static boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+    for(int i=0; i<nums.length; i++) {
+      if (set.contains(nums[i])) {
+        return true;
+      }
+      set.add(nums[i]);
+    }
+    return false;
+  }
+}
