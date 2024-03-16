d9b8b5b62f0e8112790461351d7ee7cc9a0d4770
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-16 19:58:34 +0530

    Update java_code.java

--- programs/maxProfit/java_code.java
+++ programs/maxProfit/java_code.java
@@ -1,23 +1,29 @@
 package programs.maxProfit;
 
-class java_code {
-    public static int maxProfit(int[] prices) {
+class java_code 
+{
+    public static int maxProfit(int[] prices) 
+    {
         int maxProfit = 0;
         int leftIndex = 0, rightIndex = 1;
         int currentProfit = 0;
-        while (rightIndex < prices.length) {
-            if (prices[rightIndex] > prices[leftIndex]) {
+        while (rightIndex < prices.length) 
+        {
+            if (prices[rightIndex] > prices[leftIndex]) 
+            {
                 currentProfit = prices[rightIndex] - prices[leftIndex];
                 maxProfit = Math.max(currentProfit, maxProfit);
-            } else {
+            } 
+            else
                 leftIndex = rightIndex;
-            }
+
             rightIndex++;
         }
         return maxProfit;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int profit = maxProfit(new int[] { 7, 6, 4, 3, 1 });
         System.out.println(profit);
     }
