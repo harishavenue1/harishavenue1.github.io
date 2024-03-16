a008fa53f539357790fbf988d39b9309b495d1f6
Harish Kumar P <harishkp@Harishs-Mini.lan>
2024-03-16 19:55:20 +0530

    Indentation

--- programs/containsDuplicate/csharp_code.cs
+++ programs/containsDuplicate/csharp_code.cs
@@ -7,25 +7,28 @@ namespace HelloWorld
 	{
 		public static void Main(string[] args)
 		{
-		  int[] arr = {1,2,3,1};
-			Console.WriteLine("containsDuplicate "+ containsDuplicate(arr));
-			
-			arr = new int[] {1,2,3,4};
-			Console.WriteLine("containsDuplicate "+ containsDuplicate(arr));
-			
-			arr = new int[] {1,1,1,3,3,4,3,2,4,2};
-			Console.WriteLine("containsDuplicate "+ containsDuplicate(arr));
+			int[] arr = { 1, 2, 3, 1 };
+			Console.WriteLine("containsDuplicate " + containsDuplicate(arr));
+
+			arr = new int[] { 1, 2, 3, 4 };
+			Console.WriteLine("containsDuplicate " + containsDuplicate(arr));
+
+			arr = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+			Console.WriteLine("containsDuplicate " + containsDuplicate(arr));
 		}
-		
-		public static Boolean containsDuplicate(int[] nums) {
-		  HashSet<int> inSet = new HashSet<int>();
-		  for(int i=0; i<nums.Length; i++) {
-		    if (inSet.Contains(nums[i])) {
-		      return true;
-		    }
-		    inSet.Add(nums[i]);
-		  }
-		  return false;
+
+		public static Boolean containsDuplicate(int[] nums)
+		{
+			HashSet<int> inSet = new HashSet<int>();
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (inSet.Contains(nums[i]))
+				{
+					return true;
+				}
+				inSet.Add(nums[i]);
+			}
+			return false;
 		}
 	}
 }
