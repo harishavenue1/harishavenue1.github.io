83e0fea7e8873ee8fed1f68d31a1d47260ba1db2
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-16 19:59:49 +0530

    Update java_code.java

--- programs/palindrome/java_code.java
+++ programs/palindrome/java_code.java
@@ -1,17 +1,19 @@
 package programs.palindrome;
 
-public class java_code {
-    public static boolean isPalindrome(String str) {
+public class java_code 
+{
+    public static boolean isPalindrome(String str) 
+    {
         String reversed = new StringBuilder(str).reverse().toString();
         return str.equals(reversed);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         String str = "radar";
-        if (isPalindrome(str)) {
+        if (isPalindrome(str))
             System.out.println(str + " is a palindrome.");
-        } else {
+        else
             System.out.println(str + " is not a palindrome.");
-        }
     }
 }
