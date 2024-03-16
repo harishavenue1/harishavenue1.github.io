fd58e2a62f5bc6c31096e5af5aa753d22b57cd10
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-16 20:13:22 +0530

    Update java_code.java

--- programs/maxSumOfSubArray/java_code.java
+++ programs/maxSumOfSubArray/java_code.java
@@ -1,6 +1,8 @@
 
-class java_code {
-    public static void main(String[] args) {
+class java_code 
+{
+    public static void main(String[] args) 
+    {
         int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
         System.out.println("Max Sum of Sub Array " + maxSumOfSubArray(arr));
 
@@ -11,12 +13,14 @@ class java_code {
         System.out.println("Max Sum of Sub Array " + maxSumOfSubArray(arr));
     }
 
-    public static int maxSumOfSubArray(int[] nums) {
+    public static int maxSumOfSubArray(int[] nums) 
+    {
         int arrLen = nums.length;
         int currSum = nums[0];
         int maxSum = nums[0];
 
-        for (int i = 1; i < arrLen; i++) {
+        for (int i = 1; i < arrLen; i++) 
+        {
             currSum += nums[i];
             if (currSum < 0)
                 currSum = 0;
