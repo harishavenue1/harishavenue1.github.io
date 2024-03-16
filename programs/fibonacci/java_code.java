cc7128a9d90e6de997cea052b6d4e1c9262a5112
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-16 20:00:11 +0530

    Update java_code.java

--- programs/fibonacci/java_code.java
+++ programs/fibonacci/java_code.java
@@ -1,11 +1,14 @@
 package programs.fibonacci;
 
-class java_code {
-    public static void main(String[] args) {
+class java_code 
+{
+    public static void main(String[] args) 
+    {
         int n = 10; // Change the value of n for different number of Fibonacci numbers
         int a = 0, b = 1, sum = 0;
         System.out.println("Fibonacci Series:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             System.out.print(a + " ");
             sum = a + b;
             a = b;
