71224fc8d3e2e04e31935a94ff7d2eaf4c6c349d
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-16 19:59:15 +0530

    Update java_code.java

--- programs/twosum/java_code.java
+++ programs/twosum/java_code.java
@@ -4,20 +4,25 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-class java_code {
-    public static void main(String[] args) {
+class java_code 
+{
+    public static void main(String[] args) 
+    {
         int[] array = new int[] { 1, 23, 4, 5, 6 };
         int target = 9;
         int[] output = twoSum(array, target);
         System.out.println(Arrays.toString(output)); // return index of twosum=target
     }
 
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) 
+    {
         Map<Integer, Integer> numMap = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) 
+        {
             int key = target - nums[i];
-            if (numMap.containsKey(key)) {
+            if (numMap.containsKey(key)) 
+            {
                 return new int[] { numMap.get(key), i };
             }
             numMap.put(nums[i], i);
