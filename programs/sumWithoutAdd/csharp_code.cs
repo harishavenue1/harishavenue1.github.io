3c6a2e298ac92d6b29663c57c618ae54c80d821f
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-21 21:56:37 +0530

    Create csharp_code.cs

---
+++ programs/sumWithoutAdd/csharp_code.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace HelloWorld
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		  int a = 1, b = 2;
+      Console.WriteLine("Sum of A & B is: "+ sumWithoutAdd(a, b));
+      
+      a = 2; b = 3;
+      Console.WriteLine("Sum of A & B is: "+ sumWithoutAdd(a, b));
+		}
+		
+		public static int sumWithoutAdd(int a, int b) 
+    {
+      int temp = 0;
+      while (b != 0) 
+      {
+        temp = (a & b) << 1; // and oper and carry left 1 
+        a = a ^ b; // xor oper 
+        b = temp;
+      }
+      return a;
+    } 
+	}
+}
