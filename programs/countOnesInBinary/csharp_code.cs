eaa032452d3b2a86664b93d0706d4eee507dab6c
Harish Kumar P <harish.avenue1@gmail.com>
2024-03-22 21:43:02 +0530

    Create csharp_code.cs

---
+++ programs/countOnesInBinary/csharp_code.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace HelloWorld
+{
+	public class Program
+	{
+		public static void Main(String[] args) 
+		{
+      bitWiseAndCount(11);
+      bitWiseAndCount(128);
+      bitWiseAndCount(2147483645);
+    }
+    
+    public static void bitWiseAndCount(int num) 
+    {
+      int setBit = 0;
+      while (num != 0) {
+        setBit += num & 1;
+        num = num >> 1;
+      }
+      Console.WriteLine("Set Bit: "+ setBit);
+    }
+	}
+}
