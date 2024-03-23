1ffbe532d1df31d847b3582b89352d228ceaec6a
Harish Kumar P <harishkp@Harishs-Mac-mini.local>
2024-03-23 22:22:42 +0530

    indented

--- programs/searchInRotatedSortedArray/csharp_code.cs
+++ programs/searchInRotatedSortedArray/csharp_code.cs
@@ -2,32 +2,32 @@ using System;
 
 namespace HelloWorld
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			int[] arr = {4,5,6,7,0,1,2};
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
       int target = 0;
       int targetIndex = searchInRotatedSortedArray(arr, target);
-      Console.WriteLine("Target index is "+ targetIndex);
-      
+      Console.WriteLine("Target index is " + targetIndex);
+
       target = 3;
       targetIndex = searchInRotatedSortedArray(arr, target);
-      Console.WriteLine("Target index is "+ targetIndex);
-      
-      arr = new int[] {1};
+      Console.WriteLine("Target index is " + targetIndex);
+
+      arr = new int[] { 1 };
       target = 0;
       targetIndex = searchInRotatedSortedArray(arr, target);
-      Console.WriteLine("Target index is "+ targetIndex);
-		}
-		
-		public static int searchInRotatedSortedArray(int[] nums, int target) 
+      Console.WriteLine("Target index is " + targetIndex);
+    }
+
+    public static int searchInRotatedSortedArray(int[] nums, int target)
     {
       int leftIndex = 0;
       int rightIndex = nums.Length - 1;
       int midIndex = 0;
-      
-      while (leftIndex < rightIndex - 1) 
+
+      while (leftIndex < rightIndex - 1)
       {
         midIndex = (leftIndex + rightIndex) / 2;
         if (nums[midIndex] == target)
@@ -39,5 +39,5 @@ namespace HelloWorld
       }
       return -1;
     }
-	}
+  }
 }
